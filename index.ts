7dee0a3cb574c4f5cc51f5059356097b6c365c8c
flebedev77 <flebedev77@gmail.com>
2024-12-28 23:29:50 +1030

    Good

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log("YESS!!");
+  console.log(req.body);
+  res.json({ ok: true });
+});
+
+app.get("/:info", (req, res) => {
+  console.log(req.params.info);
+  res.send("ok");
+});
+
+//@ts-ignore
+app.listen(PORT, console.log(`Server listening on port ${PORT}`));
